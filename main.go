8fa381f104088387197b179c3bae03139be01f03
saviobarr <saviobarr@gmail.com>
2024-01-01 08:48:15 -0300

    adding bubble sort algorithm

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
 	bubbleSort()
 }
 func bubbleSort() {
 	var scores = []int{90, 70, 50, 80, 60, 85}
+	fmt.Println(scores)
 	var length = len(scores)
-	for i := 0; i < length-1; i++ {
-		var isSwap = false
-		for j := 0; j < length-i-1; j++ {
-			if scores[j] > scores[j+1] {
-				var flag = scores[j]
-				scores[j] = scores[j+1]
-				scores[j+1] = flag
-				isSwap = true
+	for i := 0; i < length; i++ {
+		sorted := false
+		for j := i + 1; j < length; j++ {
+			if scores[i] > scores[j] {
+				minor := scores[j]
+				scores[j] = scores[i]
+				scores[i] = minor
+				sorted = true
 			}
 		}
-		if isSwap == false {
+		if !sorted {
 			break
 		}
+
 	}
+	fmt.Println(scores)
 }
